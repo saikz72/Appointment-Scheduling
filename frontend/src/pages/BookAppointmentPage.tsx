522ee908a078560c38dd3c2320ac12f3fe0943be
Saikou Ceesay <ceesaysaikou72@gmail.com>
2022-01-12 00:03:46 -0500

    appointment booking [WIP]

--- frontend/src/pages/BookAppointmentPage.tsx
+++ frontend/src/pages/BookAppointmentPage.tsx
@@ -1,12 +1,129 @@
+import { Box, MenuItem, Paper, TextField, Button } from '@mui/material';
+import React from 'react';
 import Navbar from '../components/Navbar';
 import { useAuth } from '../utility/AuthProvider';
 
 export default function BookAppointmentPage() {
   const { user } = useAuth();
-  console.log(user);
+  const [currency, setCurrency] = React.useState('EUR');
+  const [date, setDate] = React.useState(Date.now);
+  console.log(date);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCurrency(event.target.value);
+  };
+
+  const handleClick = () => {};
+
+  const currencies = [
+    {
+      value: 'USD',
+      label: '$',
+    },
+    {
+      value: 'EUR',
+      label: '€',
+    },
+    {
+      value: 'BTC',
+      label: '฿',
+    },
+    {
+      value: 'JPY',
+      label: '¥',
+    },
+  ];
   return (
     <div>
       <Navbar />
+      <Box
+        mx={50}
+        mt={4}
+        component={Paper}
+        sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}
+      >
+        <Box mt={2} component="form">
+          <TextField
+            id="outlined-select-currency"
+            select
+            label="Select"
+            value={currency}
+            onChange={handleChange}
+            helperText="Please select your currency"
+          >
+            {currencies.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+          <TextField
+            id="outlined-select-currency"
+            select
+            label="Select"
+            value={currency}
+            onChange={handleChange}
+            helperText="Please select your currency"
+          >
+            {currencies.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+          <Box></Box>
+        </Box>
+        <Box
+          sx={{
+            display: 'grid',
+            gap: 1,
+            gridTemplateColumns: 'repeat(2, 1fr)',
+          }}
+        >
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Button variant="contained" onClick={handleClick}>
+            Book Appointment
+          </Button>
+        </Box>
+      </Box>
     </div>
   );
 }
