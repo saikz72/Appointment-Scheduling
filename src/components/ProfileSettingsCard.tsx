7f7427b4ce4c13dcece2d9e3cbe9f04adeffc676
Saikou Ceesay <ceesaysaikou72@gmail.com>
2022-02-01 15:50:40 -0500

    updates

--- src/components/ProfileSettingsCard.tsx
+++ src/components/ProfileSettingsCard.tsx
@@ -3,7 +3,7 @@ import Box from '@mui/material/Box';
 import { alpha } from '@mui/material/styles';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
-import { Button, Collapse, TextField } from '@mui/material';
+import { Button, Collapse, Modal, TextField, Typography } from '@mui/material';
 import BusinessType from '../types/BusinessType';
 import { updateBusinessInfo } from '../services/BusinessService';
 import * as actions from '../utility/action';
@@ -12,8 +12,21 @@ import profilePicture from '../assets/img_avatar.png';
 import { useAuth } from '../utility/AuthProvider';
 import { usePersist } from '../utility/PersistenceProvider';
 
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
 export default function ProfileSettingsCard() {
   const [expanded, setExpanded] = React.useState(false);
+  const [openDeleteModal, setOpenDeleteModal] = React.useState(false);
   const persist = usePersist();
   const auth = useAuth();
   const user = auth.user ? auth.user : persist.user;
@@ -44,6 +57,34 @@ export default function ProfileSettingsCard() {
   };
 
   const handleAccountDelete = () => {};
+
+  const DeleteModal = (props: any) => {
+    return (
+      <Modal
+        open={openDeleteModal}
+        onClose={() => setOpenDeleteModal(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Are you sure you want delete this service?
+          </Typography>
+          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+            <Button
+              onClick={() => {
+                handleAccountDelete();
+                setOpenDeleteModal(false);
+              }}
+            >
+              Yes
+            </Button>
+            <Button onClick={() => setOpenDeleteModal(false)}>No</Button>
+          </Box>
+        </Box>
+      </Modal>
+    );
+  };
   return (
     <Box
       sx={{
@@ -119,7 +160,7 @@ export default function ProfileSettingsCard() {
             variant="contained"
             color="error"
             startIcon={<DeleteForeverIcon />}
-            onClick={handleAccountDelete}
+            onClick={() => setOpenDeleteModal(true)}
           >
             Delete Account
           </Button>
