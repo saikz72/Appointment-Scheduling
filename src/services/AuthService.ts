9c04ae7de329d12251e61f0796547c1cb984bdb1
Saikou Ceesay <ceesaysaikou72@gmail.com>
2022-02-05 01:28:17 -0500

    technician card

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -10,3 +10,53 @@ export const getUserInformation = async (requestBody: any) => {
     throw error;
   }
 };
+
+export const createTechnician = async (requestBody: any) => {
+  try {
+    const response = await fetch(baseURL + '/auth/register', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(requestBody),
+    });
+    if (!response.ok) {
+      const message: string = `An error has occured: ${response.status}`;
+      throw new Error(message);
+    }
+    return await response.json();
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getAllTechnicians = async () => {
+  try {
+    const response = await fetch(baseURL + '/auth/getAllTechnicians');
+    if (!response.ok) {
+      const message: string = `An error has occured: ${response.status}`;
+      throw new Error(message);
+    }
+    return await response.json();
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteTechnician = async (requestBody: any) => {
+  console.log(requestBody);
+  try {
+    const response = await fetch(baseURL + '/auth/deleteAccount', {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(requestBody),
+    });
+
+    if (!response.ok) {
+      const message: string = `An error has occured: ${response.status}`;
+      throw new Error(message);
+    }
+    return await response.json();
+  } catch (err) {
+    console.log('err', err);
+    throw err;
+  }
+};
